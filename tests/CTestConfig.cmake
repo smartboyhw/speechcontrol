97293961805b619d2a2ea9e1ebf2cf00a133bfb7
Jacky Alcine <jackyalcine@gmail.com>
2012-03-07 08:13:56 -0500

    Set the CDash drop location to http://thesii.org/Cdash, where it's currently installed.

--- tests/CTestConfig.cmake
+++ tests/CTestConfig.cmake
@@ -8,7 +8,7 @@
 set(CTEST_PROJECT_NAME "SpeechControl")
 set(CTEST_NIGHTLY_START_TIME "12:00:00 UTC")
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "dev.thesii.org/cdash")
+set(CTEST_DROP_SITE "thesii.org/Cdash")
 set(CTEST_DROP_LOCATION "/submit.php?project=spchcntrl")
 set(CTEST_DROP_SITE_CDASH TRUE)
 set(CTEST_OUTPUT_ON_FAILURE ON)

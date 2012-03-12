99e2f47d7ffa8c1ff42b94e6f3b3291035302051
Jacky Alcine <jackyalcine@gmail.com>
2012-03-11 21:48:51 -0400

    Added the 'uninstall' target to the build system.

--- cmake/Modules/SpeechControlDefaults.cmake
+++ cmake/Modules/SpeechControlDefaults.cmake
@@ -9,3 +9,11 @@ set(SPCHCNTRL_PLUGINS_LIB_INSTALL_DIR "${SPCHCNTRL_LIB_DIR}/speechcontrol")
 set(SPCHCNTRL_PLUGINS_SPEC_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/share/speechcontrol/plugins")
 set(SPCHCNTRL_APP_DATA "${CMAKE_INSTALL_PREFIX}/share/speechcontrol")
 set(SPCHCNTRL_DOC_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/share/doc")
+
+########### Add uninstall target ###############
+CONFIGURE_FILE(
+  "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+  "${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake"
+  IMMEDIATE @ONLY)
+ADD_CUSTOM_TARGET(uninstall
+  "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake")

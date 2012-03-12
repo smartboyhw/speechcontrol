1ee18db8f0a14f2793348bfa08526618ab983793
Jacky Alcine <jackyalcine@gmail.com>
2012-03-12 11:04:03 -0400

    Renamed 'dsktpcntrl' to 'desktopctl'.

---
+++ src/plugins/desktopctl/syscntrl/src/plugin.cpp
@@ -0,0 +1,53 @@
+/***
+ *  This file is part of SpeechControl.
+ *
+ *  Copyright (C) 2012 SpeechControl Developers <spchcntrl-devel@thesii.org>
+ *            (C) 2012 Jacky Alcine <jacky.alcine@thesii.org>
+ *
+ *  SpeechControl is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  SpeechControl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with SpeechControl .  If not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#include <QDebug>
+#include <QtPlugin>
+
+#include "config.hpp"
+#include "plugin.hpp"
+#include "commands.hpp"
+
+APPLIST_NAMESPACE_BEGIN
+
+Plugin::Plugin ( QObject* parent ) : AbstractPlugin ( QUuid ( PLUGIN_UUID ),parent ) {
+}
+
+void Plugin::initialize() {
+    qDebug() << "Plug-in loaded! (applist)";
+    ApplicationListCategory::instance()->addCommand ( ( new StartCommand ) );
+}
+
+void Plugin::deinitialize() {
+    qDebug() << "Plug-in unloaded! (applist)";
+    ApplicationListCategory::instance()->deleteLater();
+}
+
+Plugin::~Plugin() {
+
+}
+
+#include "plugin.moc"
+
+APPLIST_NAMESPACE_END
+
+Q_EXPORT_PLUGIN2 ( spchcntrl-applist, SpeechControl::Plugins::DesktopControl::ApplicationListing::Plugin )
+
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on; 

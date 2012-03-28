e187f73a81fe7f5b5ea1a37202ead238c0c1c932
Jacky Alcine <jackyalcine@gmail.com>
2012-03-28 02:23:37 -0400

    Made changes to CMake system, to export more CMake values.

--- cmake/Modules/SpeechControlDefaults.cmake
+++ cmake/Modules/SpeechControlDefaults.cmake
@@ -1,21 +1,26 @@
 cmake_minimum_required(VERSION 2.8)
 
-set(SPCHCNTRL_PLUGINS_LIB_INSTALL_DIR "@SPCHCNTRL_PLUGINS_LIB_INSTALL_DIR@")
-set(SPCHCNTRL_PLUGINS_SPEC_INSTALL_DIR "@SPCHCNTRL_PLUGINS_SPEC_INSTALL_DIR@")
+set(SPCHCNTRL_COMPILE_FLAGS "${QTGSTREAMER_FLAGS} -feliminate-unused-debug-symbols")
+set(SPCHCNTRL_COMPILE_FLAGS_DEBUG "-Wall -Wextra -Wabi -Wctor-dtor-privacy -Wformat -Wunused")
+set(SPCHCNTRL_COMPILE_DEFINITIONS ${QT_DEFINITIONS})
+set(SPCHCNTRL_CMAKE_DIR "${CMAKE_INSTALL_PREFIX}/share/cmake/SpeechControl")
+set(SPCHCNTRL_CMAKE_INSTALL_DIR "${CMAKE_MODULE_PATH}")
+set(SPCHCNTRL_DATA_DIR "${CMAKE_INSTALL_PREFIX}/share/speechcontrol")
+set(SPCHCNTRL_DATA_CONTENT_DIR "${SPCHCNTRL_DATA_DIR}/contents")
 set(SPCHCNTRL_LIB_DIR "${CMAKE_INSTALL_PREFIX}/lib")
 set(SPCHCNTRL_BIN_DIR "${CMAKE_INSTALL_PREFIX}/bin")
 set(SPCHCNTRL_INCLUDE_DIR "${CMAKE_INSTALL_PREFIX}/include/speechcontrol")
 set(SPCHCNTRL_PLUGINS_LIB_INSTALL_DIR "${SPCHCNTRL_LIB_DIR}/speechcontrol")
 set(SPCHCNTRL_PLUGINS_SPEC_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/share/speechcontrol/plugins")
-set(SPCHCNTRL_APP_DATA "${CMAKE_INSTALL_PREFIX}/share/speechcontrol")
 set(SPCHCNTRL_DOC_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/share/doc")
 
 ########### Add uninstall target ###############
 if (NOT TARGET uninstall)
     CONFIGURE_FILE(
-    "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
-    "${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake"
+        "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake"
     IMMEDIATE @ONLY)
+
     ADD_CUSTOM_TARGET(uninstall
-    "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake")
+                      "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake")
 endif(NOT TARGET uninstall)

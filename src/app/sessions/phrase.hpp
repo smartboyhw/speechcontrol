ab8f93a4d48a096b629d8ae51e0b48825f588ddf
Jacky Alcine <jacky.alcine@thesii.org>
2012-04-02 12:28:11 -0400

    Added documentation to SpeechControl::Phrase.

--- src/app/sessions/phrase.hpp
+++ src/app/sessions/phrase.hpp
@@ -37,8 +37,14 @@ class Corpus;
 class Phrase;
 class Corpus;
 
+/**
+ * @brief Represents a list of Phrase objects.
+ **/
 typedef QList<Phrase*> PhraseList;
 
+/**
+ * @brief Represents a phrase of text, as part of a speech corpus.
+ **/
 class SPCH_EXPORT Phrase : public QObject
 {
     Q_OBJECT
@@ -46,19 +52,56 @@ class SPCH_EXPORT Phrase : public QObject
     friend class Corpus;
 
 public:
+    /**
+     * @brief Destructor.
+     **/
     virtual ~Phrase();
+
+    /**
+     * @brief Obtains the audio QFile of this phrase.
+     **/
     QFile* audio() const;
+
+    /**
+     * @brief Obtains the text of this Phrase.
+     **/
     const QString text() const;
-    uint words() const;
-    uint index() const;
+
+    /**
+     * @brief Obtains the number of words in this Phrase.
+     **/
+    quint8 words() const;
+
+    /**
+     * @brief Obtains the index of this Phrase.
+     **/
+    quint8 index() const;
+
+    /**
+     * @brief Determines if this Phrase has been recorded.
+     **/
     bool isCompleted() const;
+
+    /**
+     * @brief Obtains the path to the audio file.
+     * @see audio()
+     **/
     QString audioPath() const;
-    static Phrase* create(Corpus* p_corpus, const QString& p_text);
+
+    /**
+     * @brief Creates a new Phrase for a corpus.
+     *
+     * Creates a new Phrase with text p_text for a specific corpus, p_corpus.
+     *
+     * @param p_corpus The corpus obtaining this new Phrase.
+     * @param p_text The QString rendering this Phrase.
+     **/
+    static Phrase* create (Corpus* p_corpus, const QString& p_text);
 
 private:
     explicit Phrase (Corpus* p_corpus, const QDomElement* p_elem);
-    const QDomElement* m_elem;
-    Corpus* m_corpus;
+    const QDomElement* m_elem;         ///< The QDomElement of the Phrase.
+    Corpus* m_corpus;                  ///< The Corpus of this Phrase.
 };
 
 }

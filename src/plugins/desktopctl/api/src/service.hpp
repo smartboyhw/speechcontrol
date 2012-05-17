46a45bcaf39d9a3503bf00425f51c229ba9318a3
Jacky Alcine <jackyalcine@gmail.com>
2012-05-17 19:05:36 -0400

    Moved all of the desktop control code to a plug-in.
    Introduced a new plug-in (spchcntrl-dsktpctl) that'll hold the API code for
    Desktop Control and the basis of it for SpeechControl.

---
+++ src/plugins/desktopctl/api/src/service.hpp
@@ -0,0 +1,68 @@
+/***
+ *  This file is part of SpeechControl.
+ *
+ *  Copyright (C) 2012 Jacky Alciné <jackyalcine@gmail.com>
+ *
+ *  SpeechControl is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  SpeechControl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with SpeechControl.  If not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef SPEECHCONTROL_DESKTOPCONTROL_SERVICE_HPP
+#define SPEECHCONTROL_DESKTOPCONTROL_SERVICE_HPP
+
+#include <app/services/module.hpp>
+#include "command.hpp"
+#include "global.hpp"
+
+DSKTPCTLAPI_NAMESPACE_BEGIN
+
+class ServicePrivate;
+
+class Service : public Services::AbstractModule
+{
+    Q_OBJECT
+    Q_DISABLE_COPY (Service)
+    SC_SINGLETON (Service)
+
+signals:
+    void commandFound (const QString& p_statement, AbstractCommand* p_command);
+    void multipleCommandsFound (const QString& p_statement, const CommandList& p_commands);
+    void noCommandsFound (const QString& p_statement);
+
+protected:
+    virtual void deinitialize();
+    virtual void initialize();
+
+public:
+    void setAcousticModel (const AcousticModel& p_acModel);
+    void setDefaultAcousticModel (const AcousticModel& acModel);
+    virtual bool isEnabled() const;
+    virtual bool isActive() const;
+    virtual QString id() const;
+    virtual QPixmap pixmap() const;
+    virtual QString name() const;
+    virtual ~Service();
+
+public slots:
+    void invokeCommand (const QString& p_text);
+
+private:
+    QScopedPointer<ServicePrivate> d_ptr;
+};
+
+
+DSKTPCTLAPI_NAMESPACE_END
+
+#endif // SPEECHCONTROL_DESKTOPCONTROL_SERVICE_HPP
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on;

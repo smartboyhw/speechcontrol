304dd15f9bb7de8752cd349733f396ca2a2432ba
Adrian Borucki <gentoolx@gmail.com>
2012-06-26 17:48:13 +0200

    Rename resources and update indicator.py

--- src/app/speechcontrol/gui/indicator.py
+++ src/app/speechcontrol/gui/indicator.py
@@ -1,11 +1,13 @@
-import res_rc
+# Import Qt resources
+import resources
+
 from PyQt4.QtGui import QIcon, QSystemTrayIcon
 
 class Indicator(QSystemTrayIcon):
     def __init__(self, parent=None):
         super(Indicator, self).__init__(parent)
         
-        self.setIcon(QIcon('../res/img/SClogonobackground.png'))
+        self.setIcon(QIcon(':/indicator/white'))
         #self.setIcon(QIcon.fromTheme('face-smile'))
 
         ## Menu here
